ef6143d679e72e86bd09186a6e0b189751fd03a5
cintia-valente <cintiavalente@live.com>
2023-04-12 17:47:46 -0300

    fix: correção de bug na rota

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Router from "./ui/routes";
+
+function App() {
+  return (
+    <Router />  
+
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app/App';
+
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+
 //import { Provider } from "react-redux";
 //import { store } from "./configuration/reduxToolkit/store";
 
