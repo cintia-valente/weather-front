7a1e5ca28c13b727f30e3a5333d643eabf91d801
cintia-valente <cintiavalente@live.com>
2023-04-21 05:01:20 -0300

    :sparkles: feat: Organização do service

---
+++ src/data/services/operations/OperationsService.ts
@@ -0,0 +1,41 @@
+import { Api } from "../ApiConfig";
+import { ApiException } from "../ErrorException";
+
+export interface State {
+  state: string;
+}
+
+export interface City {
+  cityId: number;
+  name: string;
+}
+
+const getState = async () => {
+  try {
+    const { data } = await Api().get("/states/all");
+    return data;
+  } catch (error: any) {
+    return new ApiException(error.message || "Erro ao consultar a API.");
+  }
+};
+
+const getCityByState = async (selectedState: any) => {
+  try {
+    const { data } = await Api().get(`/city/${selectedState}/all-cities`);
+    return data;
+  } catch (error: any) {
+    return new ApiException(error.message || "Erro ao consultar a API.");
+  }
+};
+
+const post = () => {};
+const updateById = () => {};
+const deleteById = () => {};
+
+export const OperationsService = {
+  getState,
+  getCityByState,
+  post,
+  updateById,
+  deleteById,
+};
