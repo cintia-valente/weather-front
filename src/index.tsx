d5ed9db029f7632a761d58bc40894e1430f6797b
cintia-valente <cintiavalente@live.com>
2023-04-21 15:25:58 -0300

    :recycle: refactor: Remoção de comentários

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import reportWebVitals from './reportWebVitals';
-import { App } from './App';
-
-//import { Provider } from "react-redux";
-//import { store } from "./configuration/reduxToolkit/store";
+import reportWebVitals from "./reportWebVitals";
+import { App } from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <App />  
+    <App />
   </React.StrictMode>
 );
 
