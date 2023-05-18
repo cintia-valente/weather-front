b2efe5d5fd49c6bc862a2ee381c0b9d507d73f76
cintia-valente <cintiavalente@live.com>
2023-05-18 14:26:18 -0300

    :sparkles: feat: testes em andamento da lista de cidades

--- src/pages/RegisterWeather/RegisterWeather.test.tsx
+++ src/pages/RegisterWeather/RegisterWeather.test.tsx
@@ -1,15 +1,11 @@
 import { render, waitFor, screen } from "@testing-library/react";
 
-import axios, { AxiosInstance } from "axios";
 import { jest } from "@jest/globals";
 
-import { OperationsService } from "../../data/services/operations/OperationsService";
 import { RegisterWeather } from ".";
-import { Api } from "../../data/services/ApiConfig";
-import { ApiException } from "../../data/services/ErrorException";
 jest.mock("../../data/services/ApiConfig");
 
-const mockData = [
+const mockCities = [
   {
     idCity: 352,
     name: "Porto Alegre",
@@ -19,21 +15,32 @@ const mockData = [
     name: "Gramado",
   },
 ];
+
+jest.mock("../../data/services/operations/OperationsService", () => ({
+  OperationsService: {
+    getCity: () => Promise.resolve(mockCities),
+  },
+}));
+
 describe("RegisterWeather Page", () => {
-  const apiMock = jest.fn();
-  const getCityMock = jest.fn();
-  beforeAll(() => {
-    jest.spyOn(axios, "get").mockResolvedValue({ data: mockData });
-    apiMock.mockReturnValue({ get: getCityMock });
-  });
-
-  afterAll(() => {
-    jest.restoreAllMocks();
-  });
-
   it("deve renderizar a página RegisterWeather", () => {
     render(<RegisterWeather />);
 
     expect(screen.getByText("Cadastro Metereológico")).toBeInTheDocument();
+  });
+
+  it("deve renderizar uma lista de cidades", async () => {
+    render(<RegisterWeather />);
+
+    await waitFor(() => {
+      const selectElement = screen.getByTestId("city-select");
+      expect(selectElement).toBeInTheDocument();
+
+      mockCities.forEach((city) => {
+        const optionElement = screen.getByText(city.name);
+        expect(optionElement).toBeInTheDocument();
+        expect(optionElement).toHaveValue(city.name);
+      });
+    });
   });
 });
